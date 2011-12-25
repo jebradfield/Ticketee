bbf5ef3dfa11fe152ef3a304847834fdb9fdc14e
codenewbie <jasonbradfield@gmail.com>
2011-12-25 06:32:13 -0800

    Added feature for ensuring that a user is signed in via a confirmation email by using email spec

---
+++ app/controllers/tickets_controller.rb
@@ -0,0 +1,53 @@
+class TicketsController < ApplicationController
+before_filter :find_project
+before_filter :find_ticket, :only => [:show,
+	                              :edit,
+				      :update,
+				      :destroy]
+  def show
+  end
+
+  def edit
+
+  end
+
+  def new
+    @ticket = @project.tickets.build
+  end
+
+  def create
+    @ticket = @project.tickets.build(params[:ticket])
+    if @ticket.save
+      flash[:notice] = "Ticket has been created."
+      redirect_to [@project, @ticket]
+    else
+      flash[:alert] = "Ticket has not been created."
+      render :action => "new"
+    end
+  end
+
+  def update
+    if @ticket.update_attributes(params[:ticket])
+      flash[:notice] = "Ticket has been udpated."
+      redirect_to [@project, @ticket]
+    else
+      flash[:alert] = "Ticket has not been updated."
+      render :action => "edit"
+    end
+  end
+
+  def destroy
+    @ticket.destroy
+    flash[:notice] = "Ticket hsa been deleted."
+    redirect_to @project
+  end
+
+  private
+  def find_project
+    @project = Project.find(params[:project_id])
+  end
+
+  def find_ticket
+    @ticket = @project.tickets.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Ticketee::Application.routes.draw do
+  devise_for :users
+
 root :to => "projects#index"
 
-resources :projects
-
+resources :projects do
+  resources :tickets
+end
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
